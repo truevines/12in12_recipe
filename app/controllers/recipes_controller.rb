a618a1fddc39ac7aeed345dd19c09b6249bab21c
truevines <truevines@gmail.com>
2016-11-14 01:25:15 -0800

    Done with recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -47,7 +47,7 @@ class RecipesController < ApplicationController
 		end
 
 		def recipe_params
-			params.require(:recipe).permit(:title, :description)
+			params.require(:recipe).permit(:title, :description, :image)
 		end
 
 
